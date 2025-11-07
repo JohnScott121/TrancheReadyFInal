443794ccdde63000bfa01c2dee0a069289238614
JohnScott121 <89816904+JohnScott121@users.noreply.github.com>
2025-11-07 12:54:33 +1100

    Create site.js

---
+++ public/site/site.js
@@ -0,0 +1,10 @@
+// simple enhancements (anchor smooth scroll & future theme hook)
+(function(){
+  document.querySelectorAll('a[href^="#"]').forEach(a=>{
+    a.addEventListener('click', (e)=>{
+      const id = a.getAttribute('href').slice(1);
+      const el = document.getElementById(id);
+      if (el){ e.preventDefault(); el.scrollIntoView({ behavior:'smooth', block:'start' }); }
+    });
+  });
+})();
